b10616a01b13fc6008a8bac5cf16ab6fbd1bed94
charaf sfaoua <charaf.sfaoua.ext@orange.com>
2025-01-12 16:54:54 +0100

    new version webcome & co

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,6 @@
 (function ($) {
   "use strict";
 
-  // Spinner
-  var spinner = function () {
-    setTimeout(function () {
-      if ($('#spinner').length > 0) {
-        $('#spinner').removeClass('show');
-      }
-    }, 1);
-  };
-  spinner();
-
-
-  // Initiate the wowjs
-  new WOW().init();
-
 
   // Sticky Navbar
   $(window).scroll(function () {
@@ -67,42 +53,5 @@
     return false;
   });
 
-
-  // Testimonials carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    margin: 25,
-    dots: false,
-    loop: true,
-    center: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      992: {
-        items: 3
-      }
-    }
-  });
-
-
-  // Portfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows'
-  });
-  $('#portfolio-flters li').on('click', function () {
-    $("#portfolio-flters li").removeClass('active');
-    $(this).addClass('active');
-
-    portfolioIsotope.isotope({filter: $(this).data('filter')});
-  });
 })(jQuery);
 
